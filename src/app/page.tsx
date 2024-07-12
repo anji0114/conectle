4938e8932201ebc6bed0e803453b137933877dc8
anji0114 <anji.tnk.140114@gmail.com>
2024-07-13 07:54:05 +0900

    setting: husky,eslint,prettierの導入

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <div>
         <h1>個人開発をサポートするアプリ</h1>
       </div>
