87c4f9d6464d126170668d9ead0a47b23c4ac596
anji0114 <anji.tnk.140114@gmail.com>
2024-11-15 18:34:50 +0900

    feature: #11 buildの修正

--- src/app/api/v1/user/route.ts
+++ src/app/api/v1/user/route.ts
@@ -24,7 +24,7 @@ export const GET = async () => {
     }
 
     const { data, error } = await supabase
-      .from('users')
+      .from('profiles')
       .select('*')
       .eq('id', user.id)
       .single();
